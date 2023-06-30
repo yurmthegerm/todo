def6a7f11ace30d93737b368dcd8b1ed5981bd59
Yurim Kang <yoorim7480@gmail.com>
2023-06-30 17:36:02 -0400

    added props for responsive input display

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header() {
+function Header(props) {
   return <header>
-    <h1><a href='/'>WEB</a></h1>
+    <h1><a href='/'>{props.title}</a></h1>
   </header>
 }
 
-function Nav() {
+function Nav(props) {
+  const lst = []
+  for (let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lst.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
   return <nav>
     <ol>
-      <li><a href='/read/1'>html</a></li>
-      <li><a href='/read/2'>css</a></li>
-      <li><a href='/read/3'>js</a></li>
+      {lst}
     </ol>
   </nav>
 }
 
-function Article() {
+function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
+  const topics = [
+    {id:1, title:'title', body:'body ...'}
+  ]
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title='REACT'></Header>
+      <Nav topics={topics}></Nav>
+      <Article title='Welcome' body='Hello, WEB'></Article>
     </div>
   );
 }
