15a2cf774c03733989ce9e450e9883b8f371b4c8
Yurim Kang <yoorim7480@gmail.com>
2023-06-30 21:04:37 -0400

    added delete button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
@@ -49,6 +48,28 @@ function Create(props) {
   </article>
 }
 
+function Update(props){
+	const [title, setTitle] = useState(props.title);
+	const [body, setBody] = useState(props.body);
+	return <article>
+		<h2>Update</h2>
+		<form onSubmit={(event)=>{
+			event.preventDefault();
+			const title = event.target.title.value;
+			const body = event.target.body.value;
+			props.onUpdate(title, body);
+		}}>
+			<p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{
+				setTitle(event.target.value);
+			}}/></p>
+			<p><textarea name="body" placeholder="body" value={body} onChange={event=>{
+				setBody(event.target.value);
+			}}></textarea></p>
+			<p><input type="submit" value="Update"></input></p>
+		</form>
+	</article>
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
@@ -59,6 +80,7 @@ function App() {
     {id:3, title:'title 3', body:'body 3 ...'}
   ])
   let content = null;
+  let contextControl = null;
   if (mode === 'WELCOME') {
     content = <Article title='Welcome' body='Hello, WEB'></Article>
   } else if (mode === 'READ') {
@@ -70,7 +92,23 @@ function App() {
 				}
 			}
 		content = <Article title={title} body={body}></Article>
-	} else if (mode === 'CREATE') {
+    contextControl = <>
+      <li><a href={'/update/'+id} onClick={event => {
+        event.preventDefault();
+        setMode('UPDATE');
+      }}>UPDATE</a></li>
+      <li><input type="button" value="Delete" onClick={()=>{
+				const newTopics = [];
+				for (let i=0; i<topics.length; i++) {
+					if(topics[i].id !== id) {
+						newTopics.push(topics[i]);
+					}
+				}
+				setTopics(newTopics);
+				setMode('WELCOME');
+			}}/></li>
+    </>
+  } else if (mode === 'CREATE') {
     content = <Create onCreate={(_title, _body)=>{
       const newTopic = {id:nextId, title:_title, body:_body};
       const newTopics = [...topics];
@@ -80,7 +118,27 @@ function App() {
       setId(nextId);
       setNextId(nextId+1);
     }}></Create>
-  }
+  } else if (mode === 'UPDATE') {
+    let title, body = null;
+    for (let i=0; i<topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+		content = <Update title={title} body={body} onUpdate={(title, body)=>{
+			const newTopics = [...topics]
+			const updatedTopic = {id:id, title:title, body:body}
+			for(let i=0; i < newTopics.length; i++){
+				if(newTopics[i].id === id){
+					newTopics[i] = updatedTopic;
+					break;
+				}
+			}
+			setTopics(newTopics);
+			setMode('READ');
+		}}></Update>
+	}
 
   return (
     <div>
@@ -92,10 +150,13 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
-      <a href='/create' onClick={event => {
+      <ul>
+        <li><a href='/create' onClick={event => {
         event.preventDefault();
         setMode('CREATE');
-      }}>CREATE</a>
+        }}>CREATE</a></li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
